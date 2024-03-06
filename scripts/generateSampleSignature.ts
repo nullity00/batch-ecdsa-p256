a7d29138472811e8eba8db19482e54748f944993
nullity00 <reshmashaik3011@gmail.com>
2024-03-06 15:48:25 +0530

    generated sample signatures

--- scripts/generateSampleSignature.ts
+++ scripts/generateSampleSignature.ts
@@ -1,58 +1,64 @@
-import { computeEffEcdsaPubInput } from "@personaelabs/spartan-ecdsa"
-import { ecsign } from "@ethereumjs/util";
-import { ec } from "elliptic";
+import { p256 } from '@noble/curves/p256';
+import { SignatureType } from '@noble/curves/abstract/weierstrass';
 import crypto from "crypto";
 import fs from "fs";
 
-(BigInt.prototype as any).toJSON = function () {
-    return this.toString();
-};
+function bigint_to_array(n: number, k: number, x: bigint) {
+    let mod: bigint = 1n;
+    for (var idx = 0; idx < n; idx++) {
+      mod = mod * 2n;
+    }
+  
+    let ret: bigint[] = [];
+    var x_temp: bigint = x;
+    for (var idx = 0; idx < k; idx++) {
+      ret.push(x_temp % mod);
+      x_temp = x_temp / mod;
+    }
+    return ret;
+}
 
 const main = () => {
     /* 
     * This is a script for generating sample signatures for the sig_ecdsa circuits.
     * Useful for generating batches of random signatures when needed.
     */
-    const numSignatures = parseInt(process.argv[2]);
-    if (isNaN(numSignatures)) {
-    throw new Error("Number of signatures must be a number");
-    }
-    if (numSignatures <= 1) {
-        throw new Error("Number of signatures must be greater than 1");
-    }
-    const curve = new ec("secp256k1");
-    const privKey = Buffer.from(
-        "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-        "hex"
-    );
-    const keypair = curve.keyFromPrivate(privKey)
-    const point = keypair.getPublic();
+    const numSignatures = 10;
+    const privKey: bigint = 88549154299169935420064281163296845505587953610183896504176354567359434168161n;
+    const pubKey = p256.ProjectivePoint.fromPrivateKey(privKey);
     const inputs: any[] = [];
+    
 
     for (let i = 0; i < numSignatures; i++) {
         const msg = crypto.randomBytes(32);
-        const { v, r: _r, s } = ecsign(msg, privKey);
-        const r = BigInt("0x" + _r.toString("hex"));
-        const circuitPubInput = computeEffEcdsaPubInput(r, v, msg);
+
+        const sig: SignatureType = p256.sign(msg, privKey);
+        const r: bigint = sig.r;
+        const s: bigint = sig.s;
+        const r_array = bigint_to_array(43, 6, r);
+        const s_array = bigint_to_array(43, 6, s);
+        const pub0array = bigint_to_array(43, 6, pubKey.x);
+        const pub1array = bigint_to_array(43, 6, pubKey.y);
+        const msgArray = bigint_to_array(43, 6, BigInt('0x' + msg.toString('hex')));
+
         const input = [
-            BigInt("0x" + s.toString("hex")),
-            circuitPubInput.Tx,
-            circuitPubInput.Ty,
-            circuitPubInput.Ux,
-            circuitPubInput.Uy,
-            point.getX().toString(),
-            point.getY().toString(),
-        ]
+            r_array,
+            s_array,
+            msgArray,
+            pub0array,
+            pub1array
+        ];
+
         inputs.push(input);
     }
 
     const fileOutput = {
         "start_pub_input": inputs[0],
-        "signatures": inputs.slice(1, inputs.length)
+        "signatures": inputs.slice(1, inputs.length),
     };
 
     fs.writeFileSync(
-        "out/sig_ecdsa_batch_sample.json",
+        "scripts/sig_ecdsa_batch_sample.json",
         JSON.stringify(fileOutput)
     );
 };
